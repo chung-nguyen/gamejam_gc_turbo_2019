61c877fb73e1dc4853988bf96666c53ec0ed6e34
nmhung1210 <nmhung1210@gmail.com>
2019-06-27 15:40:31 +0900

    using wss

--- engine/source/config/production.js
+++ engine/source/config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     apiUrl: `${window.location.origin}/api`,
-    socketUrl: `ws://${window.location.host}`,
+    socketUrl: `wss://${window.location.host}`,
     //apiUrl: 'http://128.199.202.184/api',
     //socketUrl: 'ws://128.199.202.184/socket',
     localStorageName: "gamejam.rts"
